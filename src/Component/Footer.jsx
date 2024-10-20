4944299058b5f6f5598382cbc7fa17c7808213c1
yeasinmiah1272001 <yeasinmiah1272001@gmail.com>
2024-10-20 23:17:07 +0600

    social link

--- src/Component/Footer.jsx
+++ src/Component/Footer.jsx
@@ -1,4 +1,10 @@
-import { FaFacebook, FaInstagram, FaTwitter, FaLinkedin } from "react-icons/fa";
+import {
+  FaFacebook,
+  FaInstagram,
+  FaTwitter,
+  FaLinkedin,
+  FaGithub,
+} from "react-icons/fa";
 
 const Footer = () => {
   return (
@@ -6,17 +12,26 @@ const Footer = () => {
       <div className="container mx-auto text-center">
         <p className="mb-4 text-2xl font-semibold">Yeasin</p>
         <div className="flex justify-center space-x-6 mb-4">
-          <a href="https://facebook.com" className="hover:text-blue-500">
+          <a
+            href="https://github.com/yeasinmiah1272001"
+            className="hover:text-blue-500"
+          >
+            <FaGithub size={24} />
+          </a>
+          <a
+            href="https://www.linkedin.com/in/yeasin-miah-198b5829a/"
+            className="hover:text-blue-700"
+          >
+            <FaLinkedin size={24} />
+          </a>
+          <a
+            href="https://www.facebook.com/ysm.yasin.5"
+            className="hover:text-blue-500"
+          >
             <FaFacebook size={24} />
           </a>
           <a href="https://instagram.com" className="hover:text-pink-500">
             <FaInstagram size={24} />
-          </a>
-          <a href="https://twitter.com" className="hover:text-blue-400">
-            <FaTwitter size={24} />
-          </a>
-          <a href="https://linkedin.com" className="hover:text-blue-700">
-            <FaLinkedin size={24} />
           </a>
         </div>
         <p>© yeasin 2024. All Rights Reserved.</p>
