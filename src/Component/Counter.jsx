2b95756228b3f1303e079fcb6a6b2cf7bb60a12a
yeasinmiah1272001 <yeasinmiah1272001@gmail.com>
2024-10-25 21:22:35 +0600

    counter

--- src/Component/Counter.jsx
+++ src/Component/Counter.jsx
@@ -1,46 +1,31 @@
-import { useEffect } from "react";
+import React from "react";
 import CountUp from "react-countup";
-import AOS from "aos";
-import "aos/dist/aos.css";
-import img from "../assets/bg.png";
 
 const Counter = () => {
-  // Initialize AOS
-  useEffect(() => {
-    AOS.init({
-      duration: 1000, // Animation duration
-      once: true, // Ensures the animation happens only once
-    });
-  }, []);
+  const counts = [
+    { count: 10, label: "Frontend Projects" },
+    { count: 6, label: "Full Stack Projects" },
+    { count: 15, label: "Designs" },
+  ];
 
   return (
     <div
-      className="lg:py-20"
       style={{
-        backgroundImage: `url(${img})`,
-        backgroundSize: "cover",
-        backgroundPosition: "center",
-        height: "500px",
+        clipPath: "polygon(76% 0, 100% 51%, 75% 100%, 25% 100%, 0 55%, 21% 0)",
       }}
+      className="bg-btnColor py-8 px-4 flex flex-col sm:flex-row justify-center items-center sm:gap-8 gap-4"
     >
-      <div className="flex flex-col justify-center items-center text-center lg:mt-64 space-y-8 lg:space-y-0 lg:space-x-8 lg:flex-row">
-        {[
-          { count: 10, label: "Frontend Projects" },
-          { count: 6, label: "Full Stack Projects" },
-          { count: 15, label: "Designs" },
-        ].map((item, index) => (
-          <div
-            key={index}
-            className="flex flex-col items-center p-6 rounded-md w-[200px] transition-all transform hover:scale-105 hover:shadow-xl"
-            data-aos="fade-up" // AOS animation for each counter block
-          >
-            <h1 className="text-4xl font-bold text-black">
-              <CountUp end={item.count} duration={2} />+
-            </h1>
-            <span className="text-gray-600">{item.label}</span>
-          </div>
-        ))}
-      </div>
+      {counts.map((item, index) => (
+        <div
+          key={index}
+          className="text-center bg-white shadow-lg p-4 rounded-lg w-3/4 sm:w-48"
+        >
+          <h2 className="text-4xl font-bold text-primary mb-2">
+            <CountUp end={item.count} duration={2} />
+          </h2>
+          <p className="text-gray-600 text-lg">{item.label}</p>
+        </div>
+      ))}
     </div>
   );
 };
